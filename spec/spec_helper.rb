8a4c29d202c4c796d996ea964d523ce4c53b6083
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-03-08 15:49:56 +0200

    Include fakefs in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,9 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'putkitin'
 
+require 'fakefs/safe'
+require 'fakefs/spec_helpers'
+
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
