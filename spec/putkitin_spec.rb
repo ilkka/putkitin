72fd9b8007d2ffa08473ba14f7ffa2dd835f54b8
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-03-08 15:50:12 +0200

    First spec for opening tunnels

--- spec/putkitin_spec.rb
+++ spec/putkitin_spec.rb
@@ -1,7 +1,47 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+require 'socket'
+
 describe "Putkitin" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  include  FakeFS::SpecHelpers
+  
+  before :each do
+    File.open("/etc/hosts", "w") do |f|
+      f.write <<EOS
+192.168.0.1 #{Socket.gethostname}
+127.0.0.1 localhost.localdomain localhost
+::1 #{Socket.gethostname} localhost6.localdomain6 localhost6
+127.0.1.1 #{Socket.gethostname}
+
+# The following lines are desirable for IPv6 capable hosts
+::1     localhost ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+ff02::3 ip6-allhosts
+EOS
+    end
+  end
+
+  it "opens ssh tunnels" do
+    IO.should_receive(:popen) { |cmd|
+      cmd.should =~ /ssh/
+      cmd.should =~ /-L1234:example.com:1234/
+    }
+    File.read("/etc/hosts").should == <<-EOS
+192.168.0.1 #{Socket.gethostname}
+127.0.0.1 localhost.localdomain localhost example.com
+::1 #{Socket.gethostname} localhost6.localdomain6 localhost6 example.com
+127.0.1.1 #{Socket.gethostname}
+
+# The following lines are desirable for IPv6 capable hosts
+::1     localhost ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+ff02::3 ip6-allhosts
+EOS
   end
 end
