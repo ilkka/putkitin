a43ff77386c5c3e1e41f38fbbf7b3965b2905eb2
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-03-09 12:20:53 +0200

    Coupla specs for gateway

--- spec/gateway_spec.rb
+++ spec/gateway_spec.rb
@@ -1,5 +1,20 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Putkitin::Gateway do
+  include FakeFS::SpecHelpers
+
+  before :each do
+    @gw = Putkitin::Gateway.new 'example.com'
+  end
+  
+  it "takes ssh host as argument" do
+    @gw.should be_a Putkitin::Gateway
+  end
+
+  it "returns a Pipe object" do
+    pipe = @gw.pipe 'example.com', '1234'
+    pipe.should be_a Putkitin::Pipe
+  end
+
 end
 
