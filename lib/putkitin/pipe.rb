c3175283ecc6203fb4491abc2dbb1f766958ecbd
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-03-09 14:17:50 +0200

    Implement pipe constructor ssh call

--- lib/putkitin/pipe.rb
+++ lib/putkitin/pipe.rb
@@ -1,5 +1,24 @@
 module Putkitin
+
+  # This class represents an open pipe through a gateway host.
+  # One pipe for one target host / port combination.
   class Pipe
-    
+
+    # Create a new pipe.
+    #
+    # @param gw [Gateway] ssh gateway.
+    # @param host [String] target hostname.
+    # @param port [Int] port number.
+    # @return [Pipe] open pipe.
+    def initialize(gw, host, port)
+      @io = IO.popen("ssh -L#{port}:#{host}:#{port} #{gw.hostname}")
+    end
+
+    # Close this pipe.
+    #
+    def close
+    end
+
   end
+
 end
